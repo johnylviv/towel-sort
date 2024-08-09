0efaa5368caa69bd7b8e0207726888720ef68d9a
Yevheniy Hordiyenko <johnylviv@gmail.com>
2024-08-09 14:47:19 +0300

    feat: Task solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+    let arrayResult = [];
+    // Если матрица пустая, то возвращаем пустой массив
+
+    if (!matrix)
+    return [];
+
+    if (matrix.length === 0)
+    return [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 === 0) 
+        // Для чётных индексов добавляем исходный вариант
+    {
+        arrayResult = arrayResult.concat(matrix[i]);
+    }
+        else
+        // Для нечётных индексов переворачиваем массив
+        {
+        let reverseMatrix = [];
+        reverseMatrix = matrix[i].slice().reverse();
+        arrayResult = arrayResult.concat(reverseMatrix);
+        }
+    }
+  return arrayResult;
 }
